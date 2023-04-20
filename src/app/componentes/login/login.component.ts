07b100e92f6f2dd4589aef0ec6ea6671e1d6fdfe
JennyLamego2022 <jennylamego@hotmail.com>
2023-04-20 16:14:52 -0300

    feat: Criação da validação dos campos de input

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor(){ }
+  momentForm!: FormGroup
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      email: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required]),
+
+     })
+  }
+
+  get email (){
+    return this.momentForm.get('email')!;
+  }
+  get senha (){
+    return this.momentForm.get('senha')!;
+  }
+
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return ;
+    }
+  console.log("Enviou formulário")
+}
+
 
   label1Color: string = '#04183E';
   label2Color: string = '#04183E';
@@ -34,7 +62,18 @@ export class LoginComponent {
         this.label2Color='#04183E'
         break;
     }
-
   }
 
+
+  textBtn = 'Entrar'
+  botaoAtivo = true;
+
+  trocarTexto(){
+    if (this.momentForm.valid){
+      this.textBtn = 'Entrando...';
+      this.botaoAtivo = !this.botaoAtivo;
+
+      console.log(this.textBtn)
+    }
+  }
 }
