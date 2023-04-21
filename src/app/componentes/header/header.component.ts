012a24a8beba5aac0f87cf4d05482955a0c6ebd2
JennyLamego2022 <jennylamego@hotmail.com>
2023-04-21 19:50:13 -0300

    feat: Responsividade telas mobile e criação de dropdown para navbar

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css','./mq-header.component.css']
 })
 export class HeaderComponent {
 
+  botaoAtivo = true;
+
+
+  dropdownToggle(){
+    this.botaoAtivo = !this.botaoAtivo;
+    console.log("ok")
+  }
 }
